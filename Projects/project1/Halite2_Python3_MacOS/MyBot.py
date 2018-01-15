a65c8090f8663d8aa1f55d0b6b02c97d9c0104f2
derrikf2 <fleminde@mail.gvsu.edu>
2018-01-15 18:51:50 -0500

    fixed [re]assignment scheme

--- Projects/project1/Halite2_Python3_MacOS/MyBot.py
+++ Projects/project1/Halite2_Python3_MacOS/MyBot.py
@@ -20,9 +20,9 @@ while True:
         #ship not in fleet
         x = randint(1,100)
         if shipid not in attacker_fleet and shipid not in colonizer_fleet and shipid not in kamakazi_fleet:
-            if x <= 20:
+            if x <= 60:
                 attacker_fleet.append(shipid)
-            elif x <= 40:
+            elif x <= 80:
                 kamakazi_fleet.append(shipid)
             else:
                 colonizer_fleet.append(shipid)
@@ -36,11 +36,9 @@ while True:
 
         closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
 
-        closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
+        closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if (isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships)]
 
-        closest_enemy_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and entities_by_distance[distance][0].is_owned() and entities_by_distance[distance][0].owner != game_map.my_id]
-
-        #COLONIZER
+        #DEFAULT, UNCLAIMED TERRITORY
         if len(closest_empty_planets) > 0:
             target_planet = closest_empty_planets[0]
             if ship.can_dock(target_planet):
@@ -55,16 +53,28 @@ while True:
                 if navigate_command:
                     command_queue.append(navigate_command)
         else:
+            closest_enemy_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and entities_by_distance[distance][0].owner.id != game_map.my_id]
+
+            closest_dockable_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and entities_by_distance[distance][0].owner.id == game_map.my_id and entities_by_distance[distance][0].is_full() == False]
+
+            #COLONIZER
             if shipid in colonizer_fleet:
-                list(filter(lambda a: a != shipid, colonizer_fleet))
-                x = randint(1,100)
-                if x <= 75:
-                    attacker_fleet.append(shipid)
-                else:
-                    kamakazi_fleet.append(shipid)
+                if len(closest_dockable_planets) > 0:
+                    target_planet = closest_dockable_planets[0]
+                    if ship.can_dock(target_planet):
+                        command_queue.append(ship.dock(target_planet))
+                    else:
+                        navigate_command = ship.navigate(
+                                    ship.closest_point_to(target_planet),
+                                    game_map,
+                                    speed=int(hlt.constants.MAX_SPEED),
+                                    ignore_ships=False)
+
+                        if navigate_command:
+                            command_queue.append(navigate_command)
 
             #ATTACKER
-            if shipid in attacker_fleet:
+            if shipid in attacker_fleet or len(closest_dockable_planets) == 0:
                 if len(closest_enemy_ships) > 0:
                     target_ship = closest_enemy_ships[0]
                     navigate_command = ship.navigate(
@@ -81,7 +91,7 @@ while True:
                 if len(closest_enemy_planets) > 0:
                     target_planet = closest_enemy_planets[0]
                     navigate_command = ship.navigate(
-                                    ship.closest_point_to(target_planet),
+                                    target_planet,
                                     game_map,
                                     speed=int(hlt.constants.MAX_SPEED),
                                     ignore_ships=False)
