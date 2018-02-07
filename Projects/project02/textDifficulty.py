e473494b3deca6fec07ede6b45e60a8d8e588dd5
Al Duncanson <duncansa@mail.gvsu.edu>
2018-02-07 15:26:47 -0500

    replaces words with synonyms, only for short text so far

--- Projects/project02/textDifficulty.py
+++ Projects/project02/textDifficulty.py
@@ -7,8 +7,8 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 if __name__ == '__main__':
 
-	user_input = input("Enter file name: ")
-	input_string = open(user_input).read()
+	user_input = input ("Enter file name: ")
+	input_string = open (user_input).read()
 	new_string = ""
 
 	num_syllables = textstat.syllable_count(input_string)
@@ -16,22 +16,26 @@ if __name__ == '__main__':
 	num_sentences = textstat.sentence_count(input_string)
 	fk_score = textstat.flesch_reading_ease(input_string)
 
-	print ("Number of syllables: ", num_syllables)
+	print ("\nNumber of syllables: ", num_syllables)
 	print ("Number of words: ", num_words)
 	print ("Number of sentences: ", num_sentences)
-	print ("Flesch-Kincaid score: ", fk_score)
+
+	print ("\nSynonyms: ")
 
 	for word in word_tokenize(input_string):
 		synonym_set = wordnet.synsets(word.strip("."))
 		first_synonym = synonym_set[0].lemmas()[0].name()
 
 		if first_synonym:
-			print (word, "or", first_synonym)
+			if first_synonym.lower() != word.lower():
+				print ("\t", word, ":", first_synonym)
 			if textstat.syllable_count(first_synonym) < textstat.syllable_count(word):
-				#input_string.replace(word, first_synonym, inf)
 				new_string += " " + first_synonym
 			else:
 				new_string += " " +  word
 
-	print("\n\nOriginal text: ", input_string)
-	print("\n\nNew text: ", new_string)
+	print ("\n\nOriginal text: ", input_string)
+	print ("Flesch-Kincaid score: ", fk_score)
+	print ("\n\nNew text: ", new_string)
+	print ("\nNew Flesch-Kincaid score: ", textstat.flesch_reading_ease(new_string))
+	print ("\n")
