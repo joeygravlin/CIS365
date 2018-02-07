93157199854047b5de9126f77a768e50c83b4b6c
Al Duncanson <duncansa@mail.gvsu.edu>
2018-02-07 12:17:05 -0500

    fixed some stuff

--- Projects/project02/textDifficulty.py
+++ Projects/project02/textDifficulty.py
@@ -6,10 +6,14 @@ from textstat.textstat import textstat
 if __name__ == '__main__':
 
 	user_input = input("Enter file name: ")
-	input_string = open(userInput).read()
+	input_string = open(user_input).read()
 
-	num_words = textstat.lexicon_count(inputString)
-	fk_score = textstat.flesch_reading_ease(inputString)
+	num_syllables = textstat.syllable_count(input_string)
+	num_words = textstat.lexicon_count(input_string)
+	num_sentences = textstat.sentence_count(input_string)
+	fk_score = textstat.flesch_reading_ease(input_string)
 
+	print ("Number of syllables: ", num_syllables)
 	print ("Number of words: ", num_words)
+	print ("Number of sentences: ", num_sentences)
 	print ("Flesch-Kincaid score: ", fk_score)
