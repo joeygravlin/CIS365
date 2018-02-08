7d2b744a7d3a1b3e248032bc46aa8e1cd97da027
Al Duncanson <duncansa@mail.gvsu.edu>
2018-02-07 22:45:36 -0500

    fully working

--- Projects/project02/textDifficulty.py
+++ Projects/project02/textDifficulty.py
@@ -7,37 +7,50 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 if __name__ == '__main__':
 
+	# prompt user for file and open it
 	user_input = input ("Enter file name: ")
 	input_string = open (user_input).read()
-	new_string = ""
 
+	# declare/initialize lists
+	copy_string = input_string.split()
+	words_with_synonyms = []
+	the_synonyms = []
+
+	# get number of syllables, words, sentences, and FK score for the file
 	num_syllables = textstat.syllable_count(input_string)
 	num_words = textstat.lexicon_count(input_string)
 	num_sentences = textstat.sentence_count(input_string)
 	fk_score = textstat.flesch_reading_ease(input_string)
 
+	# print number of syllables, words, and sentences in the file
 	print ("\nNumber of syllables: ", num_syllables)
 	print ("Number of words: ", num_words)
 	print ("Number of sentences: ", num_sentences)
 
-	print ("\nSynonyms: ")
-
+	# loop through each token (word, comma, period) in the input file
 	for word in word_tokenize(input_string):
-		synonym_set = wordnet.synsets(word.strip("."))
-
+		# find a set of synonyms for each word
+		synonym_set = wordnet.synsets(word)
+		# if there is a synonym for that word
 		if synonym_set:
+			# get just the first synonym
 			first_synonym = synonym_set[0].lemmas()[0].name()
+			# if the synonym has less syllabes than the word
+			if textstat.syllable_count(first_synonym) < textstat.syllable_count(word):
+				words_with_synonyms.append(word)
+				the_synonyms.append(first_synonym)
 
-			if first_synonym:
-				if first_synonym.lower() != word.lower():
-					print ("\t", word, ":", first_synonym)
-					if textstat.syllable_count(first_synonym) < textstat.syllable_count(word):
-						new_string += " " + first_synonym
-				else:
-					new_string += " " +  word
+	# replace all the words with their corresponding synonyms
+	i = 0
+	while i < len(words_with_synonyms):
+		copy_string = [w.replace(words_with_synonyms[i], the_synonyms[i]) for w in copy_string]
+		i += 1
+	# join the list into one string
+	FINAL_STRING = " ".join(copy_string)
 
+	# print the original text and the new text with their FK scores
 	print ("\n\nOriginal text: ", input_string)
 	print ("Flesch-Kincaid score: ", fk_score)
-	print ("\n\nNew text: ", new_string)
-	print ("\nNew Flesch-Kincaid score: ", textstat.flesch_reading_ease(new_string))
+	print ("\n\nNew text: ", FINAL_STRING)
+	print ("\nNew Flesch-Kincaid score: ", textstat.flesch_reading_ease(FINAL_STRING))
 	print ("\n")
